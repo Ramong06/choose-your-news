b642be084fcc4bab6e74c62a9f8a63c7f8201328
RG-Mac <ramong5406@gmail.com>
2020-10-29 14:04:36 -0500

    linked all html pages through htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,10 +4,10 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../client/index.html"));
     });
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../client/index.html"));
+    app.get("/favorites", function (req, res) {
+        res.sendFile(path.join(__dirname, "../client/favorites.html"));
     });
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../client/index.html"));
+    app.get("/topic", function (req, res) {
+        res.sendFile(path.join(__dirname, "../client/topic.html"));
     });
 }
