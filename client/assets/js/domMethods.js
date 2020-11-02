d860016f926719ede71fac4bd76dbc9942c3e52d
RG-Mac <ramong5406@gmail.com>
2020-11-02 14:01:10 -0600

    added renderPlaceHolders function in domMethods.js

--- client/assets/js/domMethods.js
+++ client/assets/js/domMethods.js
@@ -1,2 +1,12 @@
 import { useIndexedDb } from "./indexedDb";
 import { formatDate, getParams } from "./utils";
+
+function renderPlaceHolders() {
+    const articleContainer = document.querySelector(".article-container");
+    const placeholders = createPlaceholders();
+
+    while (articleContainer.firstChild) {
+        articleContainer.removeChild(articleContainer.firstChild);
+    }
+    articleContainer.appendChild(placeholders);
+}
