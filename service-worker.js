3c45ced0881be093dd97d4400c34ac211cfcfaad
RG-Mac <ramong5406@gmail.com>
2020-11-02 13:33:20 -0600

    created service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/favorites.html',
+    '/topic.html',
+    '/assets/css/style.css',
+    '/dist/app.bundle.js',
+    '/dist/favorites.bundle.js',
+    '/dist/topic.bundle.js',
+];
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
