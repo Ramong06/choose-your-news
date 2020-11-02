60f8df70a672fdbca974211723b16b56e98a57ea
RG-Mac <ramong5406@gmail.com>
2020-11-02 14:40:29 -0600

    created createPlaceholder function

--- client/assets/js/domMethods.js
+++ client/assets/js/domMethods.js
@@ -1,6 +1,7 @@
 import { useIndexedDb } from "./indexedDb";
 import { formatDate, getParams } from "./utils";
 
+//Clear the article container an dinser placeholder articles
 function renderPlaceHolders() {
     const articleContainer = document.querySelector(".article-container");
     const placeholders = createPlaceholders();
@@ -9,4 +10,38 @@ function renderPlaceHolders() {
         articleContainer.removeChild(articleContainer.firstChild);
     }
     articleContainer.appendChild(placeholders);
+}
+
+//Create and return 4 placeholder articles
+function createPlaceholders() {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 4; i++) {
+        const placeholder = createPlaceholder();
+        fragment.appendChild(placeholder);
+    }
+    return fragment;
+}
+
+function createPlaceholder() {
+    return createElement(
+        "div",
+        { class: "article-skeleton" },
+        createElement(
+            "div",
+            { class: "article-skeleton__header" },
+            createElement("div", { class: "article-skeleton__title" }),
+            createElement("div", { class: "article-skeleton__published" })
+        ),
+        createElement(
+            "div",
+            { class: "article-skeleton__content" },
+            createElement("div", { class: "article-skeleton__image" }),
+            createElement("div", { class: "article-skeleton__text" }),
+            createElement("div", { class: "article-skeleton__text" }),
+            createElement("div", { class: "article-skeleton__text" }),
+            createElement("div", { class: "article-skeleton__text" }),
+            createElement("div", { class: "article-skeleton__text" })
+        )
+    );
 }
