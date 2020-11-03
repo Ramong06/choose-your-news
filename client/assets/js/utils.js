c53c6b741cf22fbc5a10777dfc361f6e1844c0e5
RG-Mac <ramong5406@gmail.com>
2020-11-03 14:39:33 -0600

    added date function to utils.js

--- client/assets/js/utils.js
+++ client/assets/js/utils.js
@@ -14,3 +14,20 @@ export function getParams() {
         return acc;
     }, {});
 }
+
+// Formats and returns date in MMMM/DD/YYYY format
+export function formatDate(dateStr) {
+  const date = new Date(dateStr);
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  return date.toLocaleDateString(options);
+}
+
+export function removeSpecialCharsFromString(str) {
+  return str.replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, "").replace(/\s/g, "");
+}
